56a87b7a4658a7b6f09331884b7ecdf58a263296
Gaku-N <naito@hosiden.com>
2024-09-08 18:56:53 +0900

    画像をモーダル画面に拡大表示できるようにした

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import styled from "styled-components";
 
 type ImageProps = {
     src: string;
@@ -13,8 +15,41 @@ type FitName = 'fill' | 'contain' | 'cover' | 'none' | 'scale-down';
 
 export const Image = (props: ImageProps) => {
     const {src, size, objectFit, alt} = props;
+    const [isOpen, setIsOpen] = useState(false);
+    const mordalOpen = () => {setIsOpen(true)};
+    const mordalClose = () => {setIsOpen(false)};
     return (
-        <img src={src} width={size.x} height={size.y}  alt={alt} style={{ objectFit: objectFit }} />
+        <>
+        <img src={src} width={size.x} height={size.y} onClick={mordalOpen} alt={alt} style={{ objectFit: objectFit }} />
+        {isOpen && (
+            <ImageModalDiv onClick={mordalClose}>
+                <ImageContent>
+                    <SImg src={src} alt={alt} />
+                </ImageContent>
+            </ImageModalDiv>
+        )}
+        </>
     )
 }
 
+const ImageModalDiv = styled.div`
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+background-color: rgba(0, 0, 0, 0.5);
+z-index: 1;
+`;
+
+const ImageContent = styled.div `
+position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const SImg = styled.img `
+    max-width: 100vw;
+    max-height: 100vh;
+`
